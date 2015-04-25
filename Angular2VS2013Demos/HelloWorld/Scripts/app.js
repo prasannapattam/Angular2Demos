8cb6e69bd07763aa7a06bc75f43fc1c52c6ef775
Prasanna Pattam <pprasannak@hotmail.com>
2015-04-25 21:52:00 +0530

    Updated to angular2.0 alpha 20

---
+++ Angular2VS2013Demos/HelloWorld/Scripts/app.js
@@ -0,0 +1,16 @@
+import {Component, View, bootstrap} from 'angular2/angular2';
+// Annotation section
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: `<h1>Hello {{ name }}</h1>`
+})
+// Component controller
+class App {
+  constructor() {
+      this.name = 'World';
+}
+}
+
+bootstrap(App);
